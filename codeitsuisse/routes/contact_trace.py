ab390e907fc922d7f9bc728d73c57c2d9a4982e0
swakv <swathkv@gmail.com>
2020-09-26 10:44:50 +0800

    init

--- codeitsuisse/routes/contact_trace.py
+++ codeitsuisse/routes/contact_trace.py
@@ -12,8 +12,5 @@ logger = logging.getLogger(__name__)
 def evaluateCT():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    logging.info("Input Data: {}".format(inputValue))
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
+    result = {}
     return json.dumps(result)
