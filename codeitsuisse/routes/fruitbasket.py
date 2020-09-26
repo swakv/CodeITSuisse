62ada666567dbf9081f85f3639b989abf5589cb1
swakv <swathkv@gmail.com>
2020-09-26 14:10:41 +0800

    MFB

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -12,6 +12,10 @@ logger = logging.getLogger(__name__)
 def evaluateMFB():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    result = {}
+    a = data['maApple']
+    w = data['maWatermelon']
+    b = data['maBanana']
+    ans = a*10 + w*20 + b*30 
+    result = ans
     logging.info("My result :{}".format(result))
     return json.dumps(result)
