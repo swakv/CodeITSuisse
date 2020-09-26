b67d6cc4c45a0656095790d7121781751a65e493
swakv <swathkv@gmail.com>
2020-09-26 11:31:42 +0800

    setup SP

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -14,5 +14,6 @@ def evaluate():
     logging.info("data sent for evaluation {}".format(data))
     n = data.get("number_of_salads")
     arr = data.get("salad_prices_street_map")
-    result = {n: arr}
+    result = n + arr
+    logging.info("My result :{}".format(result))
     return json.dumps(result)
