87288431b8371b2ec46d7d31d76b8403f4e6eff1
swakv <swathkv@gmail.com>
2020-09-26 15:43:07 +0800

    MFB

--- codeitsuisse/routes/fruit.py
+++ codeitsuisse/routes/fruit.py
@@ -1,8 +1,8 @@
 EXPORT = {
     'maApple' : 50,
-    'maRamubutan' : 60,
+    'maRamubutan' : 70,
     'maPineapple': 50,
-    'maPomegranate': 50,
+    'maPomegranate': 60,
     'maAvocado': 70,
     'maWatermelon': 40
 }
