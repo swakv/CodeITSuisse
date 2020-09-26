f20065e6a0c0b6b2a3a3b0c6a7507caee268a80c
swakv <swathkv@gmail.com>
2020-09-26 21:19:18 +0800

    SLSM

---
+++ tryclean.py
@@ -0,0 +1,24 @@
+floor = [230, 77, 222, 3, 22, 236, 194, 224]
+
+import numpy as np
+indices = [i for i, x in enumerate(floor) if x == 0]
+
+# while np.sum(floor != 0):
+# for i in range(1, len(floor)-1):
+
+count = 0
+i=0
+
+while i < len(floor) - 1:
+    if i == 0:
+        floor[i+1] = floor[i+1] - 1
+        count += 1
+        i += 1
+        continue
+    if floor[i] < floor[i-1]:
+        floor[i-1] = floor[i-1] - 1
+        count += 1
+        i -= 1
+        continue
+
+print(floor)
