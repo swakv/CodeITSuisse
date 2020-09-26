15f2dec885945d06a47977d19667f2eefd77dd7b
swakv <swathkv@gmail.com>
2020-09-26 11:34:11 +0800

    setup SP

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 @app.route('/salad-spree', methods=['POST'])
-def evaluate():
+def evaluateSP():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     n = data.get("number_of_salads")
