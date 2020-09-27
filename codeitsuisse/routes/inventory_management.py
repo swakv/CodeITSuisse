4f4a00dc718238657240318a4887223aed10b7b1
swakv <swathkv@gmail.com>
2020-09-27 09:48:41 +0800

    CT

--- codeitsuisse/routes/inventory_management.py
+++ codeitsuisse/routes/inventory_management.py
@@ -119,11 +119,12 @@ def evaluateIM():
             answers.append({"searchItemName":query_string, "searchResult":""})
         else:
             values = values.sort()
+            print(values)
             if values == None:
                 answers.append({"searchItemName":query_string, "searchResult":""})
             else: 
                 answers.append({"searchItemName":query_string, "searchResult":list(map(lambda x:x[1],values))
         })
     result = answers
-    logging.info("My result :{}".format(result))
+    # logging.info("My result :{}".format(result))
     return json.dumps(result)
