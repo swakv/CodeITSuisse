9803d65dbb23ad0e34812b97dfcddfb1b944817c
swakv <swathkv@gmail.com>
2020-09-27 10:53:41 +0800

    CT

--- codeitsuisse/routes/pretrick.py
+++ codeitsuisse/routes/pretrick.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 @app.route('/pre-trick', methods=['POST'])
-def evaluateSLSM():
+def evaluatePT():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     result = data
